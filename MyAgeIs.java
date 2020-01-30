53776b294df7938283f07ebecec09e8369eff429
cstuser <you@example.com>
2020-01-30 13:46:28 -0500

    MyAgeIs.java

---
+++ MyAgeIs.java
@@ -0,0 +1,13 @@
+public class MyAgeIs{
+
+	public static void main (String [] args){
+
+		int MyAgeIs = 10 * 2;
+
+		int CurrentYear = 1999;
+
+		System.out.println("\nI am" + " " + MyAgeIs + " " + "I was born in june" + " " + CurrentYear);
+
+		System.exit(0);
+	} 
+}
