dcde85b04f79c65466df68849cd560668aa84e39
Your Name <you@example.com>
2020-01-31 14:07:07 -0500

    Currencies.java

---
+++ Currencies.java
@@ -0,0 +1,24 @@
+public class Currencies{
+
+	public static void main( String [] args){
+
+ 
+	double Cad, Usd, Eur, CadToUsd, CadToEur;
+
+	 Cad = 5;
+	 Usd = 0.75;
+	 Eur = 0.68;
+	 CadToUsd = Cad * 0.75;
+	 CadToEur = Cad * 0.68;
+
+
+	System.out.println (" A dollar Canadian is equal to " + Usd + " " + "Usd" + " "+"and"+ " " + Eur + " " + "Eur");
+	System.out.println (" Therefore 5$ Canadian is equal to" + CadToUsd + " " + "Usd" + " " + "and"+CadToEur +" "+"Eur");
+
+	System.exit(0);
+
+
+	}
+
+	 
+}	
