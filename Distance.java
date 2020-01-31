5ea7293cb997928a5755235cd0ae7088c9f67cb7
Your Name <you@example.com>
2020-01-31 13:39:53 -0500

    Distance.java

---
+++ Distance.java
@@ -0,0 +1,16 @@
+public class Distance{
+
+	public static void main (String [] args){
+
+		int Meters = 5;
+		double Feet = 3.37;
+		double MetersToFeet = Meters * 3.37;
+
+		System.out.println ("\n A meter is equal to " + Feet + " " + "Feet");
+		System.out.println (" 5 meters is equal to " + MetersToFeet + " " + "Feet");
+
+		System.exit (0);
+
+
+	}
+}
