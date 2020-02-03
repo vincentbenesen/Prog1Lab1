7353f6e43b0ca6d76e60c8a9953e0ec195870034
Your Name <you@example.com>
2020-02-03 09:20:42 -0500

    Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle{
+
+	public static void main ( String [] args ){
+
+	double length, width, area, perimeter;
+
+	length = 4;
+	width = 6;
+	area = length * width;
+	perimeter = 2 * ( length + width );
+
+	System.out.println ("The length of the Triangle is equal to" + length + "meter" + "\n While the width of the Rectangle is equal to" + width +"meters");
+	System.out.println ("Therefore the area is equal to " + area + "m^2" + "\n While the perimeter is equal to " + perimeter + "meters");
+
+
+
+
+	}
+}
