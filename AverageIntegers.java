a591ac6d2c34a1f6e382d0dc2850eab32ed95c09
Your Name <you@example.com>
2020-02-03 10:22:34 -0500

    AverageIntegers.java

---
+++ AverageIntegers.java
@@ -0,0 +1,14 @@
+public class AverageIntegers{
+
+	public static void main ( String [] args){
+
+		int integerNumber = 1 + 7 + 9 + 34;
+		int count = 4;
+
+		double averageNumber = (double) (integerNumber) / count;
+
+		System.out.println ("The average of the numbers is equal to " + averageNumber);
+
+
+	}
+}
