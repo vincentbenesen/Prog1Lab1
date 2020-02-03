ee5f9a7202ad56134b6d77c75fb29a875554ccef
Your Name <you@example.com>
2020-02-03 10:08:11 -0500

    IntegersLoop.java

---
+++ IntegersLoop.java
@@ -0,0 +1,18 @@
+public class IntegersLoop{
+
+
+
+	public static void main ( String [] args){
+
+		int number;
+		int endValue = 9;
+		for ( number = 1; number <= 9; number++){
+
+
+		System.out.println ( " The square of " + number + " is " + (int)Math.pow(number,2));
+	
+
+		}
+
+	}
+}
